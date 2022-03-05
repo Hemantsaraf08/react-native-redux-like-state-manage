dfcd50d6d9c6df6d4ef3a782740c56dcbe78ed71
hsaraf08 <hemant.saraf@ekahal.com>
2022-03-05 17:43:38 +0530

    boiler plate code

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import IndexScreen from "./src/screens/IndexScreen";
+const navigator=createStackNavigator({
+  Index: IndexScreen
+},
+  {
+    initialRouteName: "Index",
+    defaultNavigationOptions:{
+      title:"Blog"
+    }
+  }
+)
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default createAppContainer(navigator);
